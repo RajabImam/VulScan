575dfbb68e2fa0cc10a8ef747b108faa92c309de
Rajab Imam <rajabimam@yahoo.com>
2023-02-10 17:12:02 +0100

    project refactoring

--- vulScan/vulScan/asgi.py
+++ vulScan/vulScan/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for scannerX project.
+ASGI config for vulScan project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scannerX.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vulScan.settings')
 
 application = get_asgi_application()
